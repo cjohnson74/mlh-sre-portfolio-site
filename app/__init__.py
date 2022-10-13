f63ce24a810cb4edaa979b9028ac42eacc9ddb62
Carson Johnson <cjohnson74.tech@gmail.com>
2022-10-13 12:56:01 -0400

    connected database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
 import json
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MYSQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
 
 with open('app/static/profiles.json') as f:
     parsedProfiles = json.load(f)
